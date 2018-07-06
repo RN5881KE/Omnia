6e4ff8d67ad3f7cd6e24a72baef6237243592b56
treyh <trey.hamilton21@gmail.com>
2018-07-05 21:15:28 -0500

    Semantic API Class 1.2

--- php/Semantic3_API/Semantic3.php
+++ php/Semantic3_API/Semantic3.php
@@ -7,7 +7,25 @@
  */
 require('lib/Semantics3.php');
 
+
+/***
+ * Class Semantic3
+ * THIS WILL USE THE SEMANTIC3 PRODUCT API
+ * YOU CAN FIND DOCS HERE: https://docs.semantics3.com/reference#about-the-api
+ * OR FIND THE GITHUB REPOSITORY HERE: https://github.com/Semantics3
+ */
+
+
 class Semantic3
 {
+    private $apikey;
+    private $apisecret;
+
+    // SEMANTIC3 CONTSTRUCTOR INITIALIZED WITH THE APIKEY AND APISECRET
+    function __construct($apikey=null,$apisecret=null) {
+        $this->$apikey = $apikey;
+        $this->$apisecret = $apisecret;
+
+    }
 
 }
